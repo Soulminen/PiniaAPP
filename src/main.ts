42db66af01a477069b17f24cf5d938b89ac5c0b4
Yamil Escobar <joseyamilej@hotmail.com>
2023-01-08 16:35:07 +0100

    Instalar Pinia - Primer Store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from "pinia";
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
 
-const app = createApp(App)
+import './assets/main.css';
 
-app.use(router)
+const pinia = createPinia();
 
-app.mount('#app')
+const app = createApp(App);
+
+
+app.use(pinia);
+app.use(router);
+
+
+app.mount('#app');
