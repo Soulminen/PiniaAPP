bdf10090c4e86064a64e6e43568ed45cd6013881
Yamil Escobar <joseyamilej@hotmail.com>
2023-01-08 18:06:06 +0100

    Consumo del CounterSetupStore

--- src/store/counter-setup.ts
+++ src/store/counter-setup.ts
@@ -5,17 +5,17 @@ import { defineStore } from 'pinia';
 export const useCounterSetupStore = defineStore('counterSetup', () => {
 
     const count      = ref<number>(0);
-    const lastChange = ref<Date>();
+    const lastChanged = ref<Date>();
 
     const incrementBy = ( value: number ) => {
         count.value += value;
-        lastChange.value = new Date();
+        lastChanged.value = new Date();
     }
 
     return {
         // State properties
         count,
-        lastChange,
+        lastChanged,
         //Getters
 
         squareCount: computed( () => count.value * count.value ), 
